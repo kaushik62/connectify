48f386f1ea6f63b46768158afe3319f153a3af8d
kaushik62 <kaushik62041@gmail.com>
2026-01-31 00:36:42 +0530

    changed url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:8080";
-// const BASE_URL = "https://connectify-69pj.onrender.com";
+// const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://connectify-11-1pe2.onrender.com";
 export default BASE_URL;
